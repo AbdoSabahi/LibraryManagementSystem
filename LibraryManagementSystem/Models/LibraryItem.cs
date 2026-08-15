f0b0dc04b592f47835e7ec37e094c0e94e550523
AbdelRahman Sabahy <abdosabahi30@gmail.com>
2026-08-15 12:49:42 +0300

    Fix LibraryItem implementation

--- LibraryManagementSystem/Models/LibraryItem.cs
+++ LibraryManagementSystem/Models/LibraryItem.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LibraryManagementSystem.Models
 {
-    internal class LibraryItem
+    public abstract class LibraryItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime AddedDate { get; set; }
+
+        protected LibraryItem(int id, string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title cannot be empty.", nameof(title));
+
+            Id = id;
+            Title = title;
+            AddedDate = DateTime.Now;
+        }
+
+        public abstract string GetInfo();
     }
 }
