cc1aafad17979187e2bc99d25a76403d231364c3
AbdelRahman Sabahy <abdosabahi30@gmail.com>
2026-08-15 12:56:01 +0300

    Add BorrowRecord class

---
+++ LibraryManagementSystem/Models/BorrowRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LibraryManagementSystem.Models
+{
+    public class BorrowRecord
+    {
+        public int Id { get; set; }
+        public Book Book { get; set; }
+        public Member Member { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public BorrowRecord(int id, Book book, Member member)
+        {
+            Id = id;
+            Book = book ?? throw new ArgumentNullException(nameof(book));
+            Member = member ?? throw new ArgumentNullException(nameof(member));
+            BorrowDate = DateTime.Now;
+            ReturnDate = null;
+        }
+
+        public bool IsLate()
+        {
+            if (ReturnDate.HasValue)
+                return false;
+
+            return (DateTime.Now - BorrowDate).TotalDays > 14;
+        }
+    }
+}
