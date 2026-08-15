658f6c20cafc48b1ef640428b4784e225a7e1525
AbdelRahman Sabahy <abdosabahi30@gmail.com>
2026-08-15 12:54:57 +0300

    Add PremiumMember class

---
+++ LibraryManagementSystem/Models/PremiumMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibraryManagementSystem.Models
+{
+    public class PremiumMember : Member
+    {
+        public int MaxBorrowLimit { get; set; }
+        public int LoanDays { get; set; }
+
+        public PremiumMember(int id, string name, string email)
+            : base(id, name, email)
+        {
+            MaxBorrowLimit = 10;
+            LoanDays = 30;
+        }
+
+        public override string GetInfo()
+        {
+            return $"[{Id}] {Name} (Premium) - {Email} - Joined: {JoinDate:yyyy-MM-dd} " +
+                   $"- Limit: {MaxBorrowLimit} books - Loan period: {LoanDays} days";
+        }
+    }
+}
